f07b9c708894abf0fe088ed5fab0da7c35bd3ecf
wonseok-do <wsdo970@gmail.com>
2022-08-04 03:02:05 +0900

    App추가

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import TodoList from "./pages/TodoList";
+import Detail from "./pages/Detail";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<TodoList />} />
+      <Route path="/detail/:id" element={<Detail />} />
+    </Routes>
   );
 }
 
